faffd40b50b57a3124bc93eb411acc1606b9759f
Robert Simpson <dev@rsimp.io>
2025-09-07 15:14:19 -0500

    Fix run_once_before_001_setup_terminal.sh

--- run_once_before_001_setup_terminal.sh
+++ run_once_before_001_setup_terminal.sh
@@ -3,6 +3,6 @@
 if [ ! -e "~/Library/Application Support/iTerm2/DynamicProfiles/Iterm2Profile.json" ]; then
   if [ ! -e "~/Library/Application Support/iTerm2/DynamicProfiles" ]; then
     mkdir -p "~/Library/Application Support/iTerm2/DynamicProfiles"
-    cp Iterm2Profile.json "~/Library/Application Support/iTerm2/DynamicProfiles/"
   fi
+  cp ./Iterm2Profile.json "~/Library/Application Support/iTerm2/DynamicProfiles/"
 fi
