ae0d3beffc476a9dc92057ae6f1ecb5e0b400268
Robert Simpson <dev@rsimp.io>
2025-09-07 15:47:23 -0500

    Update run_once_before_001_setup_terminal.sh

--- run_once_before_001_setup_terminal.sh
+++ run_once_before_001_setup_terminal.sh
@@ -5,6 +5,7 @@ if [ ! -e "~/Library/Application\ Support/iTerm2/DynamicProfiles/Iterm2Profile.j
     mkdir -p "~/Library/Application\ Support/iTerm2/DynamicProfiles"
   fi
   if [ ! -e "~/Library/Application\ Support/iTerm2/DynamicProfiles/Iterm2Profile.json" ]; then
+    cd ./.local/share/chezmoi
     ls -al
     cp "~/.local/share/chezmoi/Iterm2Profile.json" "~/Library/Application\ Support/iTerm2/DynamicProfiles/"
   fi
